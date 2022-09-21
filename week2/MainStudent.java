2c98c1af9a64538bf47f598ec050822ed2234ca4
ERNAT <98648181+Manapaly@users.noreply.github.com>
2022-09-21 14:34:18 +0600

    Update MainStudent.java

--- week2/MainStudent.java
+++ week2/MainStudent.java
@@ -1,27 +1,19 @@
 import java.util.Scanner;
 
-public class MainStudent {
+public class MainStudent{
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		Student s1 = new Time(scan.next(), scan.nextInt());	
+		Student s1 = new Student("Ernat", 2025);	
 		
-        System.out.println(s1.toString())
-
+        System.out.println(s1.getName());
+        System.out.println(s1.getId());
+        System.out.println(s1.getYear());
+        s1.Increment_year(15);
+        s1.print();
 
  		Student s2 = new Student();
 		
-		t2.setHour(15);
-		t2.setMinute(98);
-		t2.setSecond(62);
-		System.out.println(t1.Standard());
-		System.out.println(t2.Universal());
-		t1.add(t2);
-		t1.fixTime();
-// 		t2.setTime();
-		
-		System.out.println(t1.Standard());
-		System.out.println(t2.Universal());
 	}
 }
