359bdbceb680e1aa6939a05b5e64f02ae5c521eb
ERNAT <98648181+Manapaly@users.noreply.github.com>
2022-09-21 16:12:38 +0600

    Update MainData.java

--- Lab1/MainData.java
+++ Lab1/MainData.java
@@ -4,20 +4,20 @@ public class Main{
 	public static void main(String[] args) {
 
 		Data D = new Data();
-		boolean ok = true;
-		while(ok){
+		Analyzer A = new Analyzer();
+		
+		while(true){
 		    Scanner sc = new Scanner(System.in);
 		    String S = sc.nextLine();
-		    if(S!="Q"){
-		        D.AddData(Integer.parseInt(S));
+		    if(!S.equals("Q")){
+		        break;
 		    }else{
-		        ok = false;
+		        D.AddData(Double.parseDouble(S));
 		    }
 		}
-		
-        System.out.println(Analyzer.GetAvarageFromData(D));
-        System.out.println(Analyzer.GetMaximumFromData(D));
 
-		
+        System.out.println(A.GetAvarageFromData(D));
+        System.out.println(A.GetMaximumFromData(D));
+
 	}
 }
