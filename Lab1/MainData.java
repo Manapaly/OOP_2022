88a3a7e811b014f6f3bfa4a42fdce6638e506d40
ERNAT <98648181+Manapaly@users.noreply.github.com>
2022-09-21 15:16:20 +0600

    Update MainData.java

--- Lab1/MainData.java
+++ Lab1/MainData.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Main{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+		Data D = new Data();
+		String S;
+		while(S!="Q"){
+		    Scanner scan = new Scanner(System.in);
+		    D.add(scan.nextInt());
+		}
+		
+        System.out.printf(Analyzer.GetAvarageFromData(D));
+        System.out.printf(Analyzer.GetMaximumFromData(D));
+
+		
+	}
+}
