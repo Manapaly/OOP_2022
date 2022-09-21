183164b1b8c7d426d8832389b68d17427afd2cef
ERNAT <98648181+Manapaly@users.noreply.github.com>
2022-09-21 15:16:51 +0600

    Create Data.java

---
+++ Lab1/Data.java
@@ -0,0 +1,19 @@
+public class Data {
+	private double LargestDigit = Integer.MIN_VALUE;
+	private int NumOfDigits = 0;
+	private double SumOfDigits = 0;
+	
+	Data(){
+	}
+	
+	public void AddData(double Digit){
+	    if(Digit > LargestDigit){
+	        LargestDigit = Digit;
+	    }
+	    SumOfDigits+=data;
+	    Num0fDigits++;
+	}
+
+	
+	
+}
