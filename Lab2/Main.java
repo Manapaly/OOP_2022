f33640c9722dfa85898c406fabd16a80b3b7f727
ERNAT <98648181+Manapaly@users.noreply.github.com>
2022-09-23 14:51:15 +0600

    Update and rename a to Main.java

---
+++ Lab2/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Cabinet C1 = new Cabinet("OOP_CLASS",269, "Pakita", 32);
+	    Cabinet C2 = new Cabinet("ADS_CLASS",465, "BEISENBEK", 98);
+	    Cabinet C3 = new Cabinet("ICT_CLASS",444, "Chardon", 28);
+	    Cabinet C4 = new Cabinet("DB_CLASS",305, "Aqerke", 22);
+	    Cabinet C5 = new Cabinet("PE_CLASS",100, "Odincov", 33);
+	    
+	    Cabinet C6 = new Cabinet("CLASS",267, "Ernat", 34);
+	   System.out.println(C2.toString());
+	
+	}
+}
